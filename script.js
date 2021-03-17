19cd33705d95aa409dee980a9bd23c6eff4a0faa
Pritish0173 <pnair.pritish0173@gmail.com>
2021-03-17 09:47:37 +0530

    Script change

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
-const keys = document.querySelectorAll('.key');
+// const keys = document.querySelectorAll('.key');
 
-keys.forEach(key => {
-    key.addEventListener('click', () => playNote(key))
-})
+// keys.forEach(key => {
+//     key.addEventListener('click', () => playNote(key))
+// })
 
 
-let playNote = (key) => {
-    const noteSound = document.getElementById(key.dataset.note)
-    noteSound.currentTime = 0
-    noteSound.play()
-    key.classList.add('active')
-    noteSound.addEventListener('ended', () => {
-        key.classList.remove('active')
-    })
-}
+// let playNote = (key) => {
+//     const noteSound = document.getElementById(key.dataset.note)
+//     noteSound.currentTime = 0
+//     noteSound.play()
+//     key.classList.add('active')
+//     noteSound.addEventListener('ended', () => {
+//         key.classList.remove('active')
+//     })
+// }
